49570f5d0e1fb373a19cdee5ecbf13e01ec1b638
godlin <bogodline@gmail.com>
2023-11-15 18:50:53 +0100

    Ref Finance Example MVP

---
+++ Near/near-ref-finance/project.ts
@@ -0,0 +1,64 @@
+import {
+  NearDatasourceKind,
+  NearHandlerKind,
+  NearProject,
+} from "@subql/types-near";
+
+const project: NearProject = {
+  specVersion: "1.0.0",
+  name: "near-subql-starter",
+  version: "0.0.1",
+  runner: {
+    node: {
+      name: "@subql/node-near",
+      version: "*",
+    },
+    query: {
+      name: "@subql/query",
+      version: "*",
+    },
+  },
+  description:
+    "This is an example project that indexes price oracle feeds from the NEAR blockchain using SubQuery",
+  repository: "https://github.com/subquery/near-subql-starter",
+  schema: {
+    file: "./schema.graphql",
+  },
+  network: {
+    // chainId is the EVM Chain ID, for Near Aurora this is 1313161554
+    // https://chainlist.org/chain/1313161554
+    chainId: "mainnet",
+    /**
+     * These endpoint(s) should be public non-pruned archive node
+     * We recommend providing more than one endpoint for improved reliability, performance, and uptime
+     * Public nodes may be rate limited, which can affect indexing speed
+     * When developing your project we suggest getting a private API key
+     */
+    endpoint: ["https://archival-rpc.mainnet.near.org"],
+    // Recommended to provide the HTTP endpoint of a full chain dictionary to speed up processing
+    dictionary: "https://api.subquery.network/sq/subquery/near-dictionary",
+    bypassBlocks: [81003306], // This is a missing block from the NEAR mainnet chain that we are skipping
+  },
+  dataSources: [
+    {
+      kind: NearDatasourceKind.Runtime, // We use ethereum runtime since NEAR Aurora is a layer-2 that is compatible
+      startBlock: 105757726, // You can set any start block you want here. This block was when the sweat_welcome.near address was created
+      mapping: {
+        file: "./dist/index.js",
+        handlers: [
+          {
+            handler: "handleAction",
+            kind: NearHandlerKind.Action,
+            filter: {
+              type: "FunctionCall",
+              methodName: "swap",
+              receiver: "v2.ref-finance.near",
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
+
+export default project;
---
+++ Near/near-ref-finance/src/mappings/mappingHandlers.ts
@@ -0,0 +1,44 @@
+import { Swap, Token, Pool } from "../types";
+import { NearAction } from "@subql/types-near";
+
+export async function handleAction(action: NearAction): Promise<void> {
+  // An Action can belong to either a transaction or a receipt
+  // To check which one, we can check if action.transaction is null
+  // If it is null, then it belongs to a receipt
+  if (!action.transaction) {
+    return;
+  }
+  let actions = action.action.args.toJson()["actions"];
+
+  for (let i = 0; i < actions.length; i++) {
+    Swap.create({
+      id: `${action.transaction.block_height}-${action.transaction.result.id}-${action.id}-${i}`,
+      poolId: (await getOrCreatePool(JSON.stringify(actions[i]["pool_id"]))).id,
+      firstTokenId: (
+        await getOrCreatePool(JSON.stringify(actions[i]["token_in"]))
+      ).id,
+      secondTokenId: (
+        await getOrCreatePool(JSON.stringify(actions[i]["token_out"]))
+      ).id,
+    }).save();
+    logger.warn("Swap is saved");
+  }
+}
+
+export async function getOrCreateToken(tokenid: string): Promise<Token> {
+  let token = await Token.get(tokenid);
+  if (token === undefined) {
+    token = Token.create({ id: tokenid });
+    await token.save();
+  }
+  return token;
+}
+
+export async function getOrCreatePool(poolid: string): Promise<Pool> {
+  let pool = await Token.get(poolid);
+  if (pool === undefined) {
+    pool = Token.create({ id: poolid });
+    await pool.save();
+  }
+  return pool;
+}
